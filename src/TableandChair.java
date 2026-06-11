f8d29cea2b29acb978d7bcdf9f09df72f97206b4
snimodesign <snimo192@gmail.com>
2026-06-11 03:04:39 -0700

    Initial commit

---
+++ src/TableandChair.java
@@ -0,0 +1,40 @@
+public class TableandChair {
+    public static void main (String[] args) {
+
+
+       String[] drawing ={
+           "X                          X",
+           "X                          X",
+           "X      XXXXXXXXXXX         X",
+           "X      X         X         X",
+           "XXXXXX X         X    XXXXXX",
+           "X    X X         X    X    X",
+           "X    X X         X    X    X",
+           "X    X X         X    X    X",
+
+       };
+
+        for (String line :  drawing) {
+
+             System.out.println(line);
+        };
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+};
